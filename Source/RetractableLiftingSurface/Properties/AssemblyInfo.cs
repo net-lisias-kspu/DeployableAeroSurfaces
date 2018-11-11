7d590cbfb2ee29b723c63ab8195902917816b397
Lisias T <me@lisias.net>
2018-11-11 00:33:37 -0200

    Legal Mambo Jambo

--- Source/RetractableLiftingSurface/Properties/AssemblyInfo.cs
+++ Source/RetractableLiftingSurface/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("RetractableLiftingSurface")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("© 2016 LinuxGuruGamer, 2018 LisiasT")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
